f1b33d0f6fa2801c62b22bf4f7895bc7675b2dc0
ErnaniJMJr <ernanijmjr@gmail.com>
2022-11-09 10:40:47 -0300

    Atualizções

--- include/Computador.hpp
+++ include/Computador.hpp
@@ -1,4 +1,16 @@
 #ifndef COMPUTADOR_HPP
 #define COMPUTADOR_HPP
 
+#include "Jogador.hpp"
+
+class Computador: public Jogador{
+    private:
+        int _dificuldade;
+    
+    public: 
+        void fazer_jogada();
+
+        int getDificuldade();
+}
+
 #endif

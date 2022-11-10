90154adde50cad4620397da657065ba5e31eabe6
courapedro <94083207+courapedro@users.noreply.github.com>
2022-11-10 13:43:05 -0300

    Update Mao.hpp

--- include/Mao.hpp
+++ include/Mao.hpp
@@ -1,4 +1,13 @@
 #ifndef MAO_HPP
 #define MAO_HPP
 
+#include <vector>
+
+class Mao{
+  private:
+    std::vector<Carta*> _mao;
+  
+  public:
+    int comparar_mao(std::vector<Carta*> pri_mao; std::vector<Carta*> sec_mao);
+
 #endif

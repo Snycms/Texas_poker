f4cfe21d875c4d7fb267d4e1aabb23066a2d701f
Ernani Junior <113210294+ErnaniJMJr@users.noreply.github.com>
2022-11-18 15:06:35 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
-#include "Baralho.hpp"
-#include "Carta.hpp"
-#include "Computador.hpp" 
-#include "Jogador.hpp"
-#include "Mao.hpp"
-#include "Poker_Game.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
 
-int main(){
+#include "Carta.hpp"
+#include "Mao.hpp"
+#include "Baralho.hpp"
+#include "Jogador.hpp"
+
+
+int main (){
+    Baralho baralho;
+
+    baralho.embaralha();
     
+    Jogador jogador("Ernani");
+    
+    Mao mao = jogador.getMao_jogador();
+    
+    for(int i=0; i<7; ++i){
+        baralho.distribui_carta("RIVER", jogador.getMao_jogador());
+    }
+    
+    
+    jogador.getMao_jogador().exibe_mao();
+    //jogador.desistir(2);
+    //std::cout << "Jogador: "<< jogador.getNome() << "\nFichas: " << jogador.getFichas() << "\nNumero vitoras: " << jogador.getNum_vitoria() << "\nEsta jogando:" << jogador.getEsta_jogando() << std::endl;
+    
+    
+    //mao.valor_mao();
+    
+    //std::cout << "Valor: " << carta_topo.getValor() << " Nome: " << carta_topo.getNome() << " Naipe: " << carta_topo.getNaipe();
+    
+    return 0;
 }
